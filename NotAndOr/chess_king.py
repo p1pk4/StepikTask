d5356f05f29dc6e993ff4f19836a6b502c4e0090
Bugsy <32585986+p1pk4@users.noreply.github.com>
2021-08-31 17:26:06 +0300

    omfg

--- NotAndOr/chess_king.py
+++ NotAndOr/chess_king.py
@@ -25,6 +25,7 @@ if (x1 >= x2 -1 and x1 <= x2 +1) and (y1 >= y2 -1 and y1 <= y2 +1):
 else:
     print('NO')
 '''
+
 x1 = int(input()) # первая точка
 y1 = int(input()) # первая точка
 x2 = int(input()) # вторая точка
@@ -37,3 +38,51 @@ else:
 
 # if (x1 and y1 >= x2 - 1) or (x1 and y1 <= x2 + 1) or (x1 and y1 >= y2 -1) or (x1 and y1 <= y2 +1) or (x1 and y1 >= (x2 + 1 and y2 + 1)) or (x1 and y1 <= (x2 - 1 and y2 - 1)) or (x1 or y1 >= (x2 -1 and y2 + 1)) or (x1 or y1 <= (x2 + 1 and y2 - 1)):
 # Решил как(___or____or____)and(___or____or____)
+
+'''
+# едрить решение подсмотрел +_+:
+
+a = int(input())
+b = int(input())
+a1 = int(input())
+b1 = int(input())
+if 1 <= a <= 8 and 1 <= b <= 8:
+    if a == b == 1:
+        if a1 == 2 and b1 == 1 or a1 == 2 and b1 == 2 or a1 == 1 and b1 == 2:
+            print('YES')
+        else: print('NO')
+    elif a == 8 and b == 1:
+        if a1 == 7 and b1 == 1 or a1 == 7 and b1 == 2 or a1 == 8 and b1 == 2:
+            print('YES')
+        else: print('NO')
+    elif a == 1 and b == 8:
+        if a1 == 1 and b1 == 7 or a1 == 2 and b1 == 8 or a1 == 2 and b1 == 7:
+            print('YES')
+        else: print('NO')            
+    elif a == b == 8:
+        if a1 == 7 and b1 == 8 or a1 == 8 and b1 == 7 or a1 == b1 == 7:
+            print('YES')
+        else: print('NO')
+    elif 2 <= a <= 7 and b == 1:
+        if a1 == a - 1 and b1 == b or a1 == a + 1 and b1 == b or a1 == a - 1 and b1 == b + 1 or a1 == a and b1 == b + 1 or a1 == a + 1 and b1 == b + 1:
+            print('YES')
+        else: print('NO')
+    elif a == 1 and 2 <= b <= 7:
+        if a1 == a and b1 == b - 1 or a1 == a and b1 == b + 1 or a1 == a + 1 and b1 == b - 1 or a1 == a + 1 and b1 == b or a1 == a + 1 and b1 == b + 1:
+            print('YES')
+        else: print('NO')
+    elif 2 <= a <= 7 and b == 8:
+        if a1 == a - 1 and b1 == b or a1 == a + 1 and b1 == b or a1 == a - 1 and b1 == b - 1 or a1 == a and b1 == b - 1 or a1 == a + 1 and b1 == b - 1:
+            print('YES')
+        else: print('NO')
+    elif a == 8 and 2 <= b <= 7:
+        if a1 == a and b1 == b - 1 or a1 == a and b1 == b + 1 or a1 == a - 1 and b1 == b - 1 or a1 == a - 1 and b1 == b or a1 == a - 1 and b1 == b + 1:
+            print('YES')
+        else: print('NO')    
+    elif 2 <= a <= 7 and 2 <= b <= 7:
+        if a - 1 <= a1 <= a + 1 and b - 1 <= b1 <= b + 1:
+            print('YES')
+        else: print('NO')                
+else:
+    print('NO')
+'''
